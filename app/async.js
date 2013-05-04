12a269fc5ef204e38c5de951aacecc520598712a
Richard Goater <richard.goater27@gmail.com>
2013-05-04 18:18:57 +0100

    async answers

--- app/async.js
+++ app/async.js
@@ -3,11 +3,24 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define([ 'jquery' ], function($) {
   return {
     async : function(value) {
-
+    	return {
+    		then: function(fn) {
+    			fn(value);
+    		}
+    	}
     },
 
     manipulateRemoteData : function(url) {
-
+    	return {
+    		then: function(fn) {
+    			$.get(url, function(data) {
+    				var names = data.people.map(function(person) {
+    					return person.name;
+    				});
+    				fn(names.sort());
+    			})
+    		}
+    	}
     }
   };
 });

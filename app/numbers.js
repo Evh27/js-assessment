53f494baa74aa5dae3c15204f84fcd9ace09b7d4
Richard Goater <richard.goater27@gmail.com>
2013-05-10 00:14:37 +0100

    numbers

--- app/numbers.js
+++ app/numbers.js
@@ -3,19 +3,26 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+    	var numStr = num.toString(2);
+    	var bitStr = numStr[numStr.length - bit];
+    	return parseInt(bitStr);
     },
 
     base10: function(str) {
-
+    	return parseInt(str, 2);
     },
 
     convertToBinary: function(num) {
-
+    	var str = num.toString(2);
+    	while (str.length < 8) {
+    		str = '0' + str;
+    	}
+    	return str;
     },
 
     multiply: function(a, b) {
-
+    	var places = b.toString().split('.')[1].length;
+    	return parseFloat((a * b).toFixed(places));
     }
   };
 });
